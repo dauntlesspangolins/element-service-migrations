6b2bed53ac55b1b7ee52ac0608f74a6b3cb47333
Lucas Gaio <gaio.lucas0@gmail.com>
2020-07-13 16:57:57 -0300

    Insert file for initial elements

---
+++ element-service-inserts.sql
@@ -0,0 +1,8 @@
+INSERT INTO `element_service_schema`.`element` (`ElementID`, `name`, `atomic_number`, `symbol`, `group`, `period`, `atomic_weight`, `density_grams_per_cubic_centimeter`)
+VALUES (1, 'Hydrogen', 1, 'H', '1', '1', 1.00794, 0.0000899);
+
+INSERT INTO `element_service_schema`.`element` (`ElementID`, `name`, `atomic_number`, `symbol`, `group`, `period`, `atomic_weight`, `density_grams_per_cubic_centimeter`)
+VALUES (2, 'Helium', 2, 'He', '18', '1', 4.002602, 0.0001785);
+
+INSERT INTO `element_service_schema`.`element` (`ElementID`, `name`, `atomic_number`, `symbol`, `group`, `period`, `atomic_weight`, `density_grams_per_cubic_centimeter`)
+VALUES (3, 'Lithium', 3, 'Li', '1', '2', 6.941, 0.535);
